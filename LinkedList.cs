53dd1bb35eff9f194b26cd83dea21968b437f294
Alexander Kalabin <k414bin@yandex.ru>
2021-01-28 15:58:51 +0300

    Update from Teacher

--- LinkedList.cs
+++ LinkedList.cs
@@ -8,9 +8,9 @@ namespace Lesson2
 {
     public class LinkedList : ILinkedList
     {
-        private Node _startNode;
-        private Node _endNode;
-        private int _count;
+        public  Node _startNode { get; set; }
+        public Node _endNode { get; set; }
+        public int _count { get; set; }
 
         public void AddNode(int value)
         {
@@ -19,17 +19,51 @@ namespace Lesson2
                 _startNode = new Node { Value = value };
                 _endNode = _startNode;
             }
-            _count++;
-        } 
- 
-        public void AddNodeAfter(Node node, int value)
-        {
-            var newNode = new Node { Value = value };
-            var nextItem = node.NextItem;
-            node.NextItem = newNode;
-            newNode.NextItem = nextItem;
+           else
+           {
+                _endNode.NextItem = new Node { Value = value };
+                _endNode.NextItem.PrevItem = _endNode;
+                _endNode = _endNode.NextItem;
+            }
             _count++;
         }
+        public LinkedList(params int[] list) : this()
+        {
+            int countList = 0;
+            while (countList < list.Length)
+            {
+                AddNode(list[countList]);
+                countList++;
+            }
+        }
+        public LinkedList(int value) : this()
+        {
+            AddNode(value);
+        }
+        public LinkedList()
+        {
+            _startNode = null;
+            _endNode = null;
+            _count = 0;
+        }
+        public void AddNodeAfter(Node node, int value)
+        {
+            if (node.NextItem != null)
+            {
+                var newNode = new Node { Value = value };
+                var oldNextItem = node.NextItem;
+                var oldPrevItem = node;
+                newNode.PrevItem = oldPrevItem;
+                newNode.NextItem = oldNextItem;
+
+                oldNextItem.PrevItem = newNode;
+                oldPrevItem.NextItem = newNode;
+
+                _count++;
+            }
+            else AddNode(value);
+        }
+
 
         public Node FindNode(int searchValue)
         {
@@ -43,15 +77,44 @@ namespace Lesson2
 
         public void RemoveNode(int index)
         {
-            throw new NotImplementedException();
+            if ( index == 1 )
+            {
+                RemoveNode(_startNode);
+            }
+            else if (index == _count)
+            {
+                RemoveNode(_endNode);
+            }
+            else
+            {
+                Node node = _startNode.NextItem;
+                for (int j=2; j<index; j++)
+                {
+                  node = node.NextItem;
+                }
+                RemoveNode(node);
+            }
+
+
         }
 
         public void RemoveNode(Node node)
         {
-            if (node.NextItem == null) return;
-
-            var nextItem = node.NextItem.NextItem;
-            node.NextItem = nextItem;
+            if (node == _startNode)
+            {
+                _startNode = _startNode.NextItem;
+                _startNode.PrevItem = null;
+            }
+            else if (node == _endNode)
+            {
+                _endNode = _endNode.PrevItem;
+                _endNode.NextItem = null;
+            }
+            else
+            {
+                node.PrevItem.NextItem= node.NextItem;
+                node.NextItem.PrevItem = node.PrevItem;
+            }
             _count--;
         }
         public int GetValue(Node startNode, int itemIndex)
@@ -75,6 +138,57 @@ namespace Lesson2
         {
             Console.WriteLine($"Элемент списка\t\tЗначение\n" + $"==================================");
             for (int j = 0; j < _count; j++) Console.WriteLine($"\t({j})\t\t{GetValue(node,j)}");
+        }
+        public void Print()
+        {
+            Node CurrentNode = _startNode;
+            int i = 0;
+            Console.WriteLine($"Элемент списка\t\tЗначение\n" + $"==================================");
+            while  (CurrentNode != null)
+            {
+                i++;
+                Console.Write('\t');
+                Console.Write(i);
+                Console.Write('\t');
+                Console.Write('\t');
+                Console.Write(CurrentNode.Value);
+                Console.WriteLine();
+                CurrentNode = CurrentNode.NextItem;
+            }
+            
+        }
+        public bool Equals(LinkedList linkedList)
+        {
+            var actualNode = _startNode;
+            var expectedNode = linkedList._startNode;
+            if (_count != linkedList._count) return false;
+            while (actualNode != null)
+            {
+                if (actualNode.Equals(expectedNode))
+                {
+                    return false;
+                }
+                actualNode = actualNode.NextItem;
+                expectedNode = expectedNode.NextItem;
+            }
+            return true;
+        }
+        public bool Equals(int[] expectedArray)
+        {
+            var actualNode = _startNode;
+            if (_count != expectedArray.Length)
+            {
+                return false;
+            }
+            foreach (int val in expectedArray)
+            {
+                if (actualNode.Value != val)
+                {
+                    return false;
+                }
+                actualNode = actualNode.NextItem;
+            }
+            return true;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,18 +11,21 @@ namespace Lesson2
         static void Main(string[] args)
         {
             Bsearch.BiSearch(); // запуск бинарного поиска - оформил отдельным классом
+            Console.WriteLine();
             var linkedList = new LinkedList();
             var startNode = new Node() { Value = 99 };// создание 
             linkedList.AddNode( 55);// должно создавать и добавлять, но ничего не происходит
-            linkedList.AddNodeAfter(startNode, 11);// не добавляет ! Почему не понял
+            linkedList.AddNodeAfter(startNode, 11);//  добавляет
             linkedList.AddNodeAfter(startNode, 12); // добавляет
             linkedList.AddNodeAfter(startNode, 26); // дбавляет 
             linkedList.AddNodeAfter(startNode, 27); // дбавляет 
+            linkedList.AddNode(76);
+            linkedList.Print();
+            linkedList.RemoveNode(2);
+            // linkedList.PrintList(startNode); ;// выводит на экран список
+            // linkedList.RemoveNode(startNode); // убирает
             Console.WriteLine();
-            linkedList.PrintList(startNode); ;// выводит на экран список
-            linkedList.RemoveNode(startNode); // убирает
-            Console.WriteLine();
-            linkedList.PrintList(startNode);// выводит на экран список
+            linkedList.Print();
             Console.ReadLine();
          }
         
